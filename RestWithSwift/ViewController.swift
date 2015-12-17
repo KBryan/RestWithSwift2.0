5ec0d8791e21c15a310c00b8b0fc3eac6e88df74
KBryan <kwame@3einteractive.com>
2015-12-17 12:26:23 -0500

    Stage of working with Rest API in Swift 2.0

--- RestWithSwift/ViewController.swift
+++ RestWithSwift/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        /// Use JSON Placeholder for REST API
+        let postEndPoint:String = "http://jsonplaceholder.typicode.com/posts/1"
+        guard let url = NSURL(string: postEndPoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        /// create URL request with a valid URL
+        let urlRequest = NSURLRequest(URL: url)
+        /// configure the NSURL Session
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        let completionHandler:(NSData?,NSURLResponse?,NSError?) -> Void = {
+            (data,response,error) in
+            guard let responseData = data else {
+                print("Error: did not receive response")
+                return
+            }
+            guard error == nil else {
+                print("Error: calling GET on /post")
+                print(error)
+                return
+            }
+            /// past the result as JSON,
+            let post:NSDictionary
+            do {
+                post = try NSJSONSerialization.JSONObjectWithData(responseData, options: []) as! NSDictionary
+            } catch {
+                print("error trying to convert")
+                return
+            }
+            print("The post is: " + post.description)
+            /// the post object is a dictionary
+            /// so we just access the title using "title" key
+            if let postTitle = post["title"] as? String {
+                print("The title is: " + postTitle)
+            }
+        }
+        // create the data task
+        let task = session.dataTaskWithRequest(urlRequest, completionHandler: completionHandler)
+        task.resume()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
